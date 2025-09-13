515ea3ff3d61ef0c531d5b46c2f67bc3afae5a63
mental2008 <yangly1999@gmail.com>
2025-09-13 15:47:15 +0800

    Enable lazy input tensor

--- control-groups/sd3_controlnet.py
+++ control-groups/sd3_controlnet.py
@@ -1,3 +1,5 @@
+import time
+
 import torch
 from diffusers import StableDiffusion3ControlNetPipeline
 from diffusers.models import SD3ControlNetModel, SD3MultiControlNetModel
@@ -17,10 +19,24 @@ pipe = pipe.to("cuda")
 seed = 666
 sd_generator = torch.manual_seed(seed)
 
-control_image = load_image("canny.jpg")
+control_image = load_image("imgs/canny.jpg")
 prompt = 'Anime style illustration of a girl wearing a suit.'
 negative_prompt = None
 
+# warm up
+pipe(
+    prompt, 
+    negative_prompt=negative_prompt,
+    control_image=control_image, 
+    height=1024, 
+    width=1024, 
+    num_inference_steps=28,
+    generator=sd_generator,
+    controlnet_conditioning_scale=0.7,
+    guidance_scale=1.0,
+).images[0]
+
+start_time = time.time()
 image = pipe(
     prompt, 
     negative_prompt=negative_prompt,
@@ -32,4 +48,7 @@ image = pipe(
     controlnet_conditioning_scale=0.7,
     guidance_scale=1.0,
 ).images[0]
+end_time = time.time()
+print(f"Time taken to execute model: {end_time - start_time} seconds")
+
 image.save("diffusers_sd3_controlnet.png")
